30203acce394f1010d5b81c511b7a11df90f20ff
Wilghaner Silva <wilghener@gmail.com>
2023-05-16 08:45:03 -0300

    Implementação de classes suporte para os controllers

---
+++ lib/API/utils/helpers/htttp-response.ts
@@ -0,0 +1,43 @@
+import MissingParamError from "./../erros/missingparam-error";
+import { HttpRes } from "../types/http-types";
+
+export default class HttpResponse{
+	static badRequest(paramName: string): HttpRes{
+		console.error(new MissingParamError(paramName));
+
+		return {
+			statusCode: 400,
+			body: {
+				message: `Missing param: ${paramName}`
+			}
+		};
+	}
+
+	static serverError(): HttpRes{
+		return {
+			statusCode: 500,
+			body: {
+				message: "An internal error has ocurred",
+			}
+		};
+	}
+
+	static unauthorized(message: string): HttpRes{
+		return {
+			statusCode: 401,
+			body: {
+				message,
+			}
+		};
+	}
+
+	static ok(data: object): HttpRes{
+		return {
+			statusCode: 200,
+			body: {
+				message: "ok",
+				data
+			}
+		};
+	}
+}
