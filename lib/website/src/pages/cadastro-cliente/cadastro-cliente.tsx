5ffd8eb8323dcd4561e8a8142bcb2ae85cb64527
Wilghaner Silva <wilghener@gmail.com>
2023-05-17 16:04:56 -0300

    adição dos icones

--- lib/website/src/pages/cadastro-cliente/cadastro-cliente.tsx
+++ lib/website/src/pages/cadastro-cliente/cadastro-cliente.tsx
@@ -1,8 +1,12 @@
 import "./style.css";
+import {FaRegUserCircle} from "react-icons/fa";
+import {AiOutlineMail, AiOutlineLock} from "react-icons/ai";
+import {MdOutlinePhoneInTalk} from "react-icons/md";
+
 
 function CadastroCliente () {
   return (
-    <div className="container">
+    <div className="login-container">
       <div className="content-container">
         <div className="content-banner">
           <h1>Beleza</h1>
@@ -15,11 +19,26 @@ function CadastroCliente () {
           <form>
             <h1>Cadastre-se</h1>
 
-            <input type="text" name="name" placeholder="Digite seu nome completo"/>
-            <input type="email" name="email" placeholder="username@gmail.com"/>
-            <input type="tel" name="phone-number" placeholder="Telefone"/>
-            <input type="password" name="password" placeholder="Senha"/>
-            <input type="password" name="confirm-password" placeholder="Confirmar senha"/>
+            <div className="input-container">
+              <FaRegUserCircle className="input-icon"/>
+              <input type="text" name="name" placeholder="Digite seu nome completo"/>
+            </div>
+            <div className="input-container">
+              <AiOutlineMail className="input-icon"/>
+              <input type="email" name="email" placeholder="username@gmail.com"/>
+            </div>
+            <div className="input-container">
+              <MdOutlinePhoneInTalk className="input-icon"/>
+              <input type="tel" name="phone-number" placeholder="Telefone"/>
+            </div>
+            <div className="input-container">
+              <AiOutlineLock className="input-icon"/>
+              <input type="password" name="password" placeholder="Senha"/>
+            </div>
+            <div className="input-container">
+              <AiOutlineLock className="input-icon"/>
+              <input type="password" name="confirm-password" placeholder="Confirmar senha"/>
+            </div>
           </form>
           <div className="buttons-container">
             <button id="cadastre-se">Cadastre-se</button>
