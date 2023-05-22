eee9f31211bf4f1e728679093a26bb083f2a916b
Wilghaner Silva <wilghener@gmail.com>
2023-05-22 15:16:21 -0300

    implementação do authMiddleware

---
+++ lib/API/src/middlewares/auth-middleware.ts
@@ -0,0 +1,36 @@
+import {iTokenManager} from "../utils/interfaces";
+import { HttpReq } from "../utils/types/http-types";
+import HttpResponse from "../utils/helpers/htttp-response";
+import InvalidDependencyError from "../utils/erros/invaliddependency-error";
+
+export default class AuthMiddleware {
+	constructor (private readonly tokenManager: iTokenManager){}
+  
+	verifyToken(httpRequest: HttpReq){
+		const {authorization} = httpRequest.headers;
+		const regex = new RegExp("Bearer");
+
+		if(!authorization)
+			return HttpResponse.unauthorized("Missing token");
+
+		if(typeof authorization !== "string")
+			return HttpResponse.unauthorized("Unauthorized");
+
+		if(!this.tokenManager || !this.tokenManager.verify){
+			console.error(new InvalidDependencyError("Invalid TokenManager"));
+			return HttpResponse.serverError();
+		}
+
+		const authorizationSplit = authorization.split(" ");
+
+		if(!regex.test(authorizationSplit[0]))
+			return HttpResponse.unauthorized("Invalid token");
+
+		const verifyResult = this.tokenManager.verify(authorizationSplit[1]);
+    
+		if(typeof verifyResult === "string")
+			return HttpResponse.unauthorized(verifyResult);
+
+		return verifyResult.userId;
+	}
+}
---
+++ lib/API/src/tests/auth-middleware.spec.ts
@@ -0,0 +1,138 @@
+import {iTokenManager} from "../utils/interfaces";
+import { HttpRes } from "../utils/types/http-types";
+import AuthMiddleware from "../middlewares/auth-middleware";
+
+class TokenManagerSpy implements iTokenManager {
+	public verifyReturns : string | { userId: string} = { userId: "any_id"};
+	public token = "";
+
+	verify(token: string): string | { userId: string; } {
+		this.token = token;
+		return this.verifyReturns;
+	}
+
+	generate(userId: string): string {
+		return userId;
+	}
+}
+
+const makeSut = () => {
+	const tokenManager = new TokenManagerSpy();
+	const sut = new AuthMiddleware(tokenManager);
+
+	return {sut, tokenManager};
+};
+
+
+describe("AuthMiddleware", () => {
+	test("É esperado que retorne 500 caso o TokenManager seja inválido", () => {
+		const tokenManager = {} as iTokenManager;
+		const sut = new AuthMiddleware(tokenManager);
+
+		const httpReq = {
+			body: {},
+			headers: {
+				authorization: "Bearer any_token"
+			}
+		};
+
+		const httpResponse = sut.verifyToken(httpReq) as HttpRes;
+
+		expect(httpResponse.statusCode).toEqual(500);
+	});
+
+	test("É esperado que retorne 401 e a mensagem \"token inválido\" caso o token seja inválido", () => {
+		const {sut} = makeSut();
+
+		const httpReq = {
+			body: {},
+			headers: {
+				authorization: "invalid any_token"
+			}
+		};
+
+		const httpResponse = sut.verifyToken(httpReq) as HttpRes;
+    
+		expect(httpResponse.body.message).toBe("Invalid token");
+		expect(httpResponse.statusCode).toBe(401);
+	});
+
+	test("É esperado que retorne 401 e a mensagem \"token faltando\" caso o token não seja fornecido", () => {
+		const {sut} = makeSut();
+
+		const httpReq = {
+			body: {},
+			headers: {
+			}
+		};
+
+		const httpResponse = sut.verifyToken(httpReq) as HttpRes;
+
+		expect(httpResponse.statusCode).toBe(401);
+		expect(httpResponse.body.message).toBe("Missing token");
+	});
+
+	test("É esperado que retorne 401 caso não exista do campo 'authorization' no header", () => {
+		const {sut} = makeSut();
+
+		const httpReq = {
+			body: {},
+			headers: {
+				authorization: 1
+			}
+		};
+
+		const httpResponse = sut.verifyToken(httpReq) as HttpRes;
+
+		expect(httpResponse.statusCode).toBe(401);
+		expect(httpResponse.body.message).toBe("Unauthorized");
+	});
+
+	test("É esperado que retorne 401 caso o token seja inválido ou tenha expirado", () => {
+		const {sut, tokenManager} = makeSut();
+
+		tokenManager.verifyReturns = "Expired token";
+
+		const httpReq = {
+			body: {},
+			headers: {
+				authorization: "Bearer invalid_invalid token"
+			}
+		};
+ 
+		const httpResponse = sut.verifyToken(httpReq) as HttpRes;
+
+		expect(httpResponse.statusCode).toBe(401);
+		expect(httpResponse.body.message).toBe("Expired token");
+	});
+
+	test("É esperado que retorne o id do usuário caso tudo ocorra bem", () => {
+		const {sut} = makeSut();
+
+		const httpReq = {
+			body: {},
+			headers: {
+				authorization: "Bearer any_token"
+			}
+		};
+
+		const userId = sut.verifyToken(httpReq);
+
+		expect(typeof userId === "string").toBe(true);
+	});
+
+	test("should pass the parameters correctly to the TokenManager", () => {
+		const { sut, tokenManager} = makeSut();
+
+		const httpReq = {
+			body: {},
+			headers: {
+				authorization: "Bearer any_token"
+			}
+		};
+
+		sut.verifyToken(httpReq);
+
+		expect(tokenManager.token).toEqual("any_token");
+	});
+});
