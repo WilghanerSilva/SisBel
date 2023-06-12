7a892c1a7071e30340d07d3b3f330365908d32c9
Wilghaner Silva <wilghener@gmail.com>
2023-06-12 08:51:12 -0300

    Adição de comentários

--- lib/API/src/tests/login-controller.spec.ts
+++ lib/API/src/tests/login-controller.spec.ts
@@ -4,10 +4,16 @@ import { iLoginService, iEmailValidator } from "../utils/interfaces";
 import { HttpReq } from "../utils/types/http-types";
 
 
-describe("Login Controller", () => {
+describe("Login Controller", () => {  //Definição do suite de teste, nesse caso o teste está relacioando ao Login Controller
   
+	//Função que retorna uma instância de uma classe que simula as dependências da classe testada
 	const makeLoginSVCSpy = () => {
+
+		//definição da classe false
 		class LoginSVCSpy implements iLoginService {
+      
+			//atributo que será usado como retorno na função simulada
+			//a definição desse atributo permite que nos testes o retorno da função false seja manipulado
 			public token: {profile: string, token: string} | null = {profile: "any_profile", token: "any_token"};
       
 			async auth() {
@@ -15,9 +21,12 @@ describe("Login Controller", () => {
 			}
 		}
 
+
+		//retorno de uma nova instância da classe simulada
 		return new LoginSVCSpy();
 	};
 
+	//Outra dependência simulada
 	const makeEmailValidatorSpy = () => {
 		class EmailValidatorSpy implements iEmailValidator {
 			public result = true;
@@ -30,6 +39,10 @@ describe("Login Controller", () => {
 		return new EmailValidatorSpy;
 	};
 
+	//função que retorna todas as dependências simuladas e uma nova instância da classe que está sendo testada
+	//Essa instancia da classe testada é chamada de SUT (System Under Test)
+	//As falsas dependências são criadas para que possamos testar como a classe testada se comporta de acordo com os possíveis
+	//retornos de cada dependência
 	const makeSut = () => {
 		const emailValidator = makeEmailValidatorSpy();
 		const loginService = makeLoginSVCSpy();
@@ -44,11 +57,18 @@ describe("Login Controller", () => {
 	};
 
   
-  
+	//Aqui temos um caso de teste, o teste é realizado pela função test que recebe dois parâmetros,
+	//o primeiro é a descrição do teste, no caso deste é esperado um código http 500 caso a classe seja montada
+	//com uma dependência inválida. No segundo parâmetro é passado uma arrow function assíncrona onde o teste de fato será realizado
 	test("É esperado que retorne 500 caso o emailValidator seja inválido", async () => {
-		const loginService = makeLoginSVCSpy();
-		const sut = new LoginCTER(loginService, {} as iEmailValidator);
+		const loginService = makeLoginSVCSpy(); //instanciação da dependencia falsa
 
+
+		//criação de uma nova instância da classe que está sendo testada, porém com a dependência emailValidator inválida
+		const sut = new LoginCTER(loginService, {} as iEmailValidator);
+                                                                    
+
+		//criação do objeto httpRequest que será passado como parâmetro na chamada do método run da classe testada
 		const httpRequest: HttpReq = {
 			body: {
 				email: "any_email@mail.com",
@@ -57,9 +77,13 @@ describe("Login Controller", () => {
 			headers: {}
 		};
 
+		//criação de uma variável que guardará o retorno do método testado
 		const httpResponse = await sut.run(httpRequest);
 
-		expect(httpResponse.statusCode).toBe(500);
+
+		//teste de fato, a função expect irá testar se o retorno está de acordo com o esperado
+		//Nesse caso, é esperado que o statusCode do response seja igual 500
+		expect(httpResponse.statusCode).toEqual(500);
 	});
 
 	test("É esperado que retorne 500 caso o loginService seja inválido", async () => {
